efdd0df6d4c4f248ad35f3b8b04884ca821c0511
Skiddie <domenp93@gmail.com>
2020-12-16 19:08:36 +0100

    infinite carousel

--- JS/carosel.js
+++ JS/carosel.js
@@ -1,65 +1,101 @@
-next = document.getElementById("nextButton");
-back = document.getElementById("backButton");
-var allCarosels = [["something",0],["carosel1",0]]
+const allCaroselss = {
+
+    carosel1: {
+
+        images: document.getElementById("carosel1Slide").childElementCount,
+        count: 1
+      },
+
+        carosel2: {
+
+            images: document.getElementById("carosel2Slide").childElementCount,
+            count: 1
+            }
+ };
+
+
+for (var i = 0; i < document.getElementsByClassName("nextbutton").length; i++) {
+
+    let caroselId = document.getElementsByClassName("nextbutton")[i].parentNode.id
+    let slider = document.getElementById(caroselId+"Slide");
+    slider.style.transform = "translateX(-"+ document.getElementById(caroselId).clientWidth +"px)";
+
+document.getElementsByClassName("nextbutton")[i].addEventListener('click', function(i){
 
 
 
 
- next.addEventListener("click", () =>{
-
-var caroselId = next.parentNode.id;
-var nmchild = document.getElementById(caroselId+"Slide").childElementCount;
 var slider = document.getElementById(caroselId+"Slide");
-var count = allCarosels[match(caroselId)][1] +1
-if(count>nmchild-1){
-  count = count -1
+
+console.log(caroselId,"next");
+
+allCaroselss[caroselId].count++
+
+
+if(allCaroselss[caroselId].count>allCaroselss[caroselId].images -1){
+  allCaroselss[caroselId].count--
 }
-  allCarosels[match(caroselId)][1] = count;
- console.log(count);
 
-var size = document.getElementById(caroselId).clientWidth * count
 
+var size = document.getElementById(caroselId).clientWidth * allCaroselss[caroselId].count
 
 
 
+
+  console.log(allCaroselss[caroselId].count);
 
 slider.style.transition = "all ease 0.4s";
 slider.style.transform = "translateX(-"+ size +"px)";
 
 
+  slider.addEventListener('transitionend', () => {
 
+    if (allCaroselss[caroselId].count==allCaroselss[caroselId].images -1) {
+    allCaroselss[caroselId].count=1;
+    slider.style.transition = "none";
+    slider.style.transform = "translateX(-"+ document.getElementById(caroselId).clientWidth +"px)";
+     }
+    });
 
-
-});
-
-
-
-back.addEventListener("click", () =>{
-
-var caroselId = next.parentNode.id;
-var nmchild = document.getElementById(caroselId+"Slide").childElementCount;
-var slider = document.getElementById(caroselId+"Slide");
-
-var count = allCarosels[match(caroselId)][1] -1
-if(count<0){
-  count = 0
-}
-  allCarosels[match(caroselId)][1] = count;
-console.log(count);
-var slider = document.getElementById(caroselId+"Slide");
-var size = document.getElementById(caroselId).clientWidth * count
-slider.style.transform = "translateX(-"+ size +"px)";
-
-});
 
 
 
 
-function match(id){
-  for (var i = 0; i < allCarosels.length; i++) {
-     allCarosels[i]
-     if (allCarosels[i].indexOf(id)!= -1){
-       return i
-      }
-  }
-};
+});
+
+ }
+
+
+for (var i = 0; i < document.getElementsByClassName("nextbutton").length; i++){
+    let caroselId = document.getElementsByClassName("nextbutton")[i].parentNode.id
+    document.getElementsByClassName("previousbutton")[i].addEventListener('click', function(i){
+
+    console.log(caroselId,"back");
+    var slider = document.getElementById(caroselId+"Slide");
+
+    allCaroselss[caroselId].count--
+
+    if(allCaroselss[caroselId].count<0){
+      allCaroselss[caroselId].count++
+    }
+
+
+    var size = document.getElementById(caroselId).clientWidth * allCaroselss[caroselId].count
+
+    slider.style.transition = "all ease 0.4s";
+    slider.style.transform = "translateX(-"+ size +"px)";
+
+
+    slider.addEventListener('transitionend', () => {
+
+      if (allCaroselss[caroselId].count==0) {
+      allCaroselss[caroselId].count= allCaroselss[caroselId].images -2;
+
+      slider.style.transition = "none";
+      slider.style.transform = "translateX(-"+ document.getElementById(caroselId).clientWidth*allCaroselss[caroselId].count +"px)";
+       }
+      });
+
+    });
+
+ }
